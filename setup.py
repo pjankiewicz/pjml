effac5f1c5d522a87e3397db6fde1c3824611962
Pawel Jankiewicz <p.jankiewicz@gmail.com>
2015-09-17 21:57:33 +0200

    chg

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from distutils.core import setup
+
 setup(name='pjml',
-      version='0.0.1',
-      package_dir={'pjml': ''},
-      packages=['pjml'],
-      )
+      version='0.1',
+      description='Some ML tools',
+      url='http://github.com/pjankiewicz/pjml',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      package_dir={'pjml': 'pjml'},
+      packages=['pjml',
+                'pjml.sklearn',
+                'pjml.sklearn.transformers'],
+      zip_safe=False)
