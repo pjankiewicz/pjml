2e40578d824e6a6875465c20c376269f03bb2cf3
Pawel Jankiewicz <pawel.j@webinterpret.com>
2015-10-01 09:05:18 +0200

    add file modification date

--- pjml/filesop.py
+++ pjml/filesop.py
@@ -1,6 +1,10 @@
 import os
-
+import datetime
 
 def touch(fname, times=None):
     with open(fname, 'a'):
         os.utime(fname, times)
+
+def file_modification_date(filename):
+    t = os.path.getmtime(filename)
+    return datetime.datetime.fromtimestamp(t)
