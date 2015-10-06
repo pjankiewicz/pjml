2ec3516ec7f19be58d6d045f0b4a0fb18e5136b0
Pawel Jankiewicz <pawel.j@webinterpret.com>
2015-10-06 09:48:28 +0200

    move sparse normalizer and correct it

--- pjml/sklearn/transformers/generic.py
+++ pjml/sklearn/transformers/generic.py
@@ -27,3 +27,19 @@ class Densify(BaseEstimator, TransformerMixin):
 
     def transform(self, x):
         return x.todense()
+
+
+class SparseNormalizer(BaseEstimator, TransformerMixin):
+
+    def fit(self, x, y=None):
+        self.colmaxs = np.asarray(x.max(0).todense())[0]
+        self.colmins = np.asarray(x.min(0).todense())[0]
+        return self
+
+    def transform(self, x):
+        x = x.tocsr().copy()
+        x.data = (x.data - self.colmins[x.indices]) / \
+            (self.colmaxs[x.indices] - self.colmins[x.indices])
+        x.data[np.isnan(x.data)] = 0
+        x.data = x.data.clip(0,1)
+        return x
